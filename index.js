915b1462eb53aa606e1081dc30d7ac6f68fa714e
pero4l <lilpero03@gmail.com>
2026-02-17 13:39:30 +0100

    added a check compare function

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const hashedText = async (text) => {
 
   const hashedText = await bcrypt.hash(text, saltRounds);
 
+  const check = await bcrypt.compare(text, hashedText)
+
  console.log(`Hashed Text = ${hashedText}`);
+ console.log(`Compare Hashed ${text} is = ${check}`);
+ 
 };
 
  hashedText("hello")
